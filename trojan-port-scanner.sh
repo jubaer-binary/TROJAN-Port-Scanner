387178b0f15577bfb4abd8ab928608d7c7853a5f
Jubaer Alnazi <51982912+jubaer-binary@users.noreply.github.com>
2019-08-14 15:30:06 +0800

    Update trojan-port-scanner.sh

--- trojan-port-scanner.sh
+++ trojan-port-scanner.sh
@@ -5,22 +5,28 @@ GREEN=$(echo -en '\001\033[00;32m\002')
 WHITE=$(echo -en '\001\033[01;37m\002')
 
 echo "${WHITE}
-
- _______         _                ______                      ______                                     
-(_______)       (_)              (_____ \             _      / _____)                                    
-    _  ____ ___  _ _____ ____     _____) ___   ____ _| |_   ( (____   ____ _____ ____  ____  _____  ____ 
+ _______         _                ______                      ______
+(_______)       (_)              (_____ \             _      / _____)
+    _  ____ ___  _ _____ ____     _____) ___   ____ _| |_   ( (____   ____ _____ ____  ____  _____  ____
    | |/ ___/ _ \| (____ |  _ \   |  ____/ _ \ / ___(_   _)   \____ \ / ___(____ |  _ \|  _ \| ___ |/ ___)
-   | | |  | |_| | / ___ | | | |  | |   | |_| | |     | |_    _____) ( (___/ ___ | | | | | | | ____| |    
-   |_|_|   \____| \_____|_| |_|  |_|    \___/|_|      \__)  (______/ \____\_____|_| |_|_| |_|_____|_|    
-              (__/                                                                                       
+   | | |  | |_| | / ___ | | | |  | |   | |_| | |     | |_    _____) ( (___/ ___ | | | | | | | ____| |
+   |_|_|   \____| \_____|_| |_|  |_|    \___/|_|      \__)  (______/ \____\_____|_| |_|_| |_|_____|_|
+              (__/
+
+${GREEN}Trojan Port Scanner Basically Tries To Detect All The Ports Which Could Have Been Opened By The Trojan To Establish Connection.
+Victim Can Run This Tool To Check If Any Unusual Port Is Getting Used By Any Malicious Software. Also We Have Added Details For Each
+Port So You Can Even Assume Which Trojan Is In Your System And Verify It's Existence.
+
+${RED} Press Ctrl + C to skip between the domains
+
 By Jubaer Alnazi
-Enter File Location-"
+Enter IP/Domain File Location-"
 read location
 input="$location"
 while IFS= read -r line
 do
 domains=$line
-
+echo "...................................................................."
 paste Data.txt | (
 while read col1 col2 ; do
 
@@ -33,9 +39,8 @@ else
 echo "${RED}PORT DOES NOT EXIST"
 fi)
 echo "$NMAP"
-echo "--------------------------------------------------------------------"
+echo "...................................................................."
+
 done
 )
-
 done < "$input"
-
